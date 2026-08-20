204e60453358fe0c4581d198fe60750ffe32120e
Kevin Sitek <sitek.kevin@gmail.com>
2026-08-20 18:27:40 -0400

    New loop files to facilitate masking

---
+++ 05_masking/loop_make_atlas_region_masks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#for subpath in /ix1/bchandrasekaran/krs228/data/FLT/data_denoised/sub*/; do 
+#  subid=$(basename $subpath)
+
+for subid in FLT02 FLT03 FLT04 FLT05 FLT06 FLT07 FLT08 FLT09 FLT10 FLT11 FLT12 FLT13 FLT14 FLT15 FLT17 FLT18 FLT19 FLT20 FLT21 FLT22 FLT23 FLT24 FLT25 FLT26 FLT28 FLT30; do
+  echo $subid
+  for atlas_label in tian_S2 subcort_aud carpet_dseg; do
+      echo $atlas_label
+      sbatch run_make_atlas_region_masks.sh $subid $atlas_label
+    done
+done
